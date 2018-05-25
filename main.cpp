a4fc3f9422fe08b8db91a0e9070430e832d29ec6
Alireza Heydari <alireza97hi@gmail.com>
2018-05-25 15:28:14 +0430

    parsing function completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 using namespace std;
-
+int SIZE_OF_BLOCK = 2;
+bool** parsing(bool*,int);
+void printSth(bool**);
 
 
 int main() {
@@ -12,3 +14,23 @@ string padding(){
 
 }
 
+void printSth(bool** data,int n,int m){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout << data[i][j];
+        }
+        cout << endl;
+    }
+}
+
+bool** parsing(bool* data,int data_size){
+    int number_of_blocks = (int)(data_size/SIZE_OF_BLOCK);
+    bool **blocks = new bool*[number_of_blocks];
+    for(int i = 0; i < data_size ;i++){
+        if(i % SIZE_OF_BLOCK == 0){
+            blocks[(int)(i/SIZE_OF_BLOCK)] = new bool[SIZE_OF_BLOCK];
+        }
+        blocks[(int)(i/SIZE_OF_BLOCK)][i%SIZE_OF_BLOCK] = data[i];
+    }
+    printSth(blocks,2,2);
+}
